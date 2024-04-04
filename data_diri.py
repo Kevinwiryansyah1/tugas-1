e4efb37ac918f20b1be09d3da8cdeedac0a12216
kevinwiryansyah1 <wiriansyahkevin@gmail.com>
2024-04-04 18:13:37 +0800

    first commit

---
+++ data_diri.py
@@ -0,0 +1,18 @@
+#   Variabel_variabel biodata
+
+nama = "Kevin Wiryansyah"
+umur = 17
+alamat = "JL. ponegoro gang jambu No. 18, lendang re sayang - sayang"
+pekerjaan = "pelajar"
+hobi = "olahraga dan gaming"
+
+# Menampilkan biodata
+
+print("===== BIODATA =====")
+print("Nama:", nama)
+print("Umur:", umur, "tahun")
+print("Alamat:", alamat)
+print("Pekerjaan:", pekerjaan)
+print("Hobi:", hobi)
+
+print("================================================================")
